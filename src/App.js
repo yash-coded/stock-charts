54eaed8c1444a19521c6e6ebac76a1ed758eb80b
yash <patelyash3377@gmail.com>
2020-12-30 20:06:08 -0700

    added discontinous timescale provider

--- src/App.js
+++ src/App.js
@@ -8,12 +8,15 @@ import { getData } from "./utils";
 
 function App() {
   const [price, setData] = useState(null);
-  const [symbol, setSymbol] = useState("aapl");
+  const [symbol, setSymbol] = useState("msft");
 
   useEffect(() => {
     getData(symbol).then((data) => {
       const newData = data.historical;
       newData.reverse();
+      for (let i = 0; i < newData.length; i++) {
+        newData[i].date = new Date(`${newData[i].date} 00:00:00`);
+      }
       setData(newData);
       console.log(newData);
     });
@@ -23,36 +26,40 @@ function App() {
     getData(symbol).then((data) => {
       const newData = data.historical;
       newData.reverse();
+      for (let i = 0; i < newData.length; i++) {
+        newData[i].date = new Date(`${newData[i].date} 00:00:00`);
+      }
       setData(newData);
       console.log(newData);
     });
   };
   return (
-    <div>
+    <div className="bg-gray-900">
       <div
-        className=" bg-gray-900 sticky top-0 flex items-center w-screen"
+        className=" bg-gray-900 sticky top-0 flex items-center w-screen "
         // style={{
         //   width: window.innerWidth,
         // }}
       >
-        <p className="text-white lg:text-base text-xs ml-4 flex-wrap">
-          Search a Stock by Symbol
+        <p className="text-white lg:text-base text-xs ml-4  hidden md:block">
+          Search a listed company by it's Symbol
         </p>
+        <p className="text-white  w-1/2   ml-2  md:hidden">Search a Symbol</p>
         <form onSubmit={handleSubmit}>
           <input
             value={symbol.toUpperCase()}
             onChange={(e) => setSymbol(e.target.value)}
             placeholder="Enter a Symbol"
-            className="bg-gray-700 p-2 ml-8 outline-none border-gray-500 text-gray-300 my-2 rounded-md "
+            className="bg-gray-700 lg:p-2 p-1 lg:ml-8 w-1/3  outline-none border-gray-500 text-gray-300 my-2 rounded-md "
           />
           <button
             type="submit"
-            className="border border-gray-700 text-white py-2 px-4 rounded-md ml-8 hover:bg-gray-700 outline-none"
+            className="border border-gray-700 text-white lg:py-2 lg:px-4 p-1  rounded-md lg:ml-8 ml-4 hover:bg-gray-700 outline-none"
           >
             Search
           </button>
         </form>
-        <p className="text-white ml-auto text-xs mr-2">
+        <p className="text-white ml-auto text-xs mr-2 hidden md:block">
           *Stock Data is only available in Daily Timeframe as of now
         </p>
       </div>
--- src/Chart.js
+++ src/Chart.js
@@ -8,7 +8,9 @@ import {
   CrossHairCursor,
   MouseCoordinateX,
   MouseCoordinateY,
+  ZoomButtons,
 } from "react-stockcharts/lib/coordinates";
+import { discontinuousTimeScaleProvider } from "react-stockcharts/lib/scale";
 import { OHLCTooltip } from "react-stockcharts/lib/tooltip";
 import { ChartCanvas, Chart } from "react-stockcharts";
 import { CandlestickSeries, BarSeries } from "react-stockcharts/lib/series";
@@ -18,12 +20,27 @@ import { last, timeIntervalBarWidth } from "react-stockcharts/lib/utils";
 
 class CandleStickChart extends React.Component {
   render() {
-    const { type, width, data, ratio, height } = this.props;
-    const xAccessor = (d) => new Date(`${d.date} 00:00:00`);
+    const { type, width, data: initialData, ratio, height } = this.props;
+    const { gridProps, seriesType } = this.props;
+    const xScaleProvider = discontinuousTimeScaleProvider.inputDateAccessor(
+      (d) => d.date
+    );
+    const { data, xScale, xAccessor, displayXAccessor } = xScaleProvider(
+      initialData
+    );
     const xExtents = [
       xAccessor(last(data)),
       xAccessor(data[data.length - 100]),
     ];
+    const margin = { left: 50, right: 50, top: 10, bottom: 30 };
+    const heightForGrid = window.innerHeight - 50;
+
+    const gridHeight = height - margin.top - margin.bottom;
+    const gridWidth = width - margin.left - margin.right;
+
+    const showGrid = true;
+    const yGrid = showGrid ? { innerTickSize: -1 * gridWidth } : {};
+    const xGrid = showGrid ? { innerTickSize: -1 * gridHeight } : {};
 
     return (
       <div
@@ -31,19 +48,20 @@ class CandleStickChart extends React.Component {
         style={{ position: "absolute", top: 50, bottom: 0 }}
       >
         <ChartCanvas
-          mouseMoveEvent={true}
-          panEvent={true}
-          zoomEvent={true}
-          clamp={false}
+          // mouseMoveEvent={true}
+          // panEvent={true}
+          // zoomEvent={true}
+          // clamp={false}
           ratio={ratio}
           width={width}
           height={window.innerHeight - 50}
-          margin={{ left: 50, right: 50, top: 10, bottom: 30 }}
+          margin={margin}
           type={type}
           seriesName="MSFT"
           data={data}
+          xScale={xScale}
           xAccessor={xAccessor}
-          xScale={scaleTime()}
+          displayXAccessor={displayXAccessor}
           xExtents={xExtents}
         >
           <Chart id={1} yExtents={(d) => [d.high, d.low]}>
@@ -53,6 +71,8 @@ class CandleStickChart extends React.Component {
               ticks={20}
               tickStroke="#6c7080"
               stroke="#6c7080"
+              {...gridProps}
+              {...xGrid}
             />
             <YAxis
               axisAt="left"
@@ -60,6 +80,8 @@ class CandleStickChart extends React.Component {
               ticks={20}
               tickStroke="#6c7080"
               stroke="#6c7080"
+              {...gridProps}
+              {...yGrid}
             />
             <MouseCoordinateX
               at="bottom"
@@ -76,10 +98,11 @@ class CandleStickChart extends React.Component {
               stroke={(d) => (d.close > d.open ? "#26a69a" : "#ef5350")}
               wickStroke={(d) => (d.close > d.open ? "#26a69a" : "#ef5350")}
               fill={(d) => (d.close > d.open ? "#26a69a" : "#ef5350")}
-              width={timeIntervalBarWidth(utcDay)}
+              // width={timeIntervalBarWidth(utcDay)}
             />
             <OHLCTooltip origin={[10, 10]} textFill="#d5d5d5" fontSize={14} />
           </Chart>
+
           <Chart
             id={2}
             height={150}
@@ -94,7 +117,7 @@ class CandleStickChart extends React.Component {
             />
           </Chart>
 
-          <CrossHairCursor stroke="#6c7080" />
+          <CrossHairCursor stroke="#ffff" />
         </ChartCanvas>
       </div>
     );
